33e6e58c3dd0fb45727339cbdc3141f46d49da26
ivanbakotin <ivan.bakotin1999@gmail.com>
2022-07-29 10:13:24 +0200

    more services

---
+++ GamersApp/Entities/Friend.cs
@@ -0,0 +1,11 @@
+﻿namespace GamersApp.Entities
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public int UserID1 { get; set; }
+        public virtual User User1 { get; set; }
+        public int UserID2 { get; set; }
+        public virtual User User2 { get; set; }
+    }
+}
---
+++ GamersApp/Entities/FriendRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace GamersApp.Entities
+{
+    public class FriendRequest
+    {
+        public  int Id { get; set; }
+        public  int Follower { get; set; }
+        public virtual User FollowerUser { get; set; }
+        public int Followed { get; set; }
+        public virtual User FollowedUser { get; set; }
+    }
+}
